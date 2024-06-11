41e048e0cdcf1dc17dde8ccc0036d142ab0915b9
Hoang vu <vuhoangnguyen200896@gmail.com>
2024-06-11 19:33:46 +0700

    feat: change env

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http:a2da41087150b4606be59156d42ba6d5-763723099.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
